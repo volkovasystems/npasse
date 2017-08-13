2e52286be1628a342e2bc532b6c3fb0ccd512a8b
johnlenon <johnlenonmaghanoy@gmail.com>
2017-08-13 15:16:29 +0800

    Updated test.

--- npasse-test.js
+++ npasse-test.js
@@ -5,17 +5,17 @@ const npasse = require( "./npasse.js" );
 let CALLED_ONCE = Symbol( "called-once" );
 let PASSABLE = Symbol( "passable" );
 let callback = function callback( ){
-	assert.deepEqual( this, { "hello": "world" }, "should be deeply equal" );
+	assert.deepEqual( this, { "hello": "world" }, "should be equal to { 'hello': 'world' }" );
 
-	assert.deepEqual( Array.from( arguments ), [ null, "hello", 1, 2, 3 ], "should be deeply equal" );
+	assert.deepEqual( Array.from( arguments ), [ null, "hello", 1, 2, 3 ], "should be equal to [ null, 'hello', 1, 2, 3 ]" );
 };
 
 callback.passed = function passed( ){
 	callback.apply( this, Array.from( arguments ) );
 
-	assert.deepEqual( this, { "hello": "world" }, "should be deeply equal" );
+	assert.deepEqual( this, { "hello": "world" }, "should be equal to { 'hello': 'world' }" );
 
-	assert.deepEqual( Array.from( arguments ), [ null, "hello", 1, 2, 3 ], "should be deeply equal" );
+	assert.deepEqual( Array.from( arguments ), [ null, "hello", 1, 2, 3 ], "should be equal to [ null, 'hello', 1, 2, 3 ]" );
 
 	console.log( "ok" );
 };
